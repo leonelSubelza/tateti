1760e89bb6dc7f89ec9a36eaf8c9fb8c7690b1ae
leonelSubelza <subelza150@gmail.com>
2022-02-09 17:37:14 -0300

    Se agregan avances
    Ahora se termina el juego cuando se hizo una jugada.
    Se modificó la tabla para que parezca un tablero de tateti

--- app/app.js
+++ app/app.js
@@ -1,45 +1,60 @@
 const boton = document.querySelector(".boton-reincio");
 
 let posiciones = ['00','01','02',
-                    '10','11','12',
-                    '20','21','22'];
+                  '10','11','12',
+                  '20','21','22'];
 
-let tablero = ['','','',
-                 '','','',
-                 '','',''];
+let tablero = [ ['','',''],
+                ['','',''],
+                ['','',''] ];
 
 let posDisponibles = ['00','01','02',
-                        '10','11','12',
-                        '20','21','22'];
+                      '10','11','12',
+                      '20','21','22'];
 
+let terminoJuego = false;
 
 document.querySelectorAll(".casillero").forEach( casilla => {
     casilla.addEventListener('click', (e) => {
         
         //en el caso de que se modifique el html a la fuerza y se escriba algo
-        if(casilla.innerHTML == '' && casilla.innerHTML != 'O' && casilla.innerHTML != 'X'){
+        if(casilla.innerHTML == '' && casilla.innerHTML != 'O' && casilla.innerHTML != 'X' && !terminoJuego){
             escribirCasilla(casilla,'X');
-            setTimeout( () => {
-                botJugar();
-            },500);
+            if(posDisponibles.length > 0 && !verificarGanador('X')){
+                setTimeout( () => {
+                    botJugar();
+    
+                },500);
+                
+                if(verificarGanador('O')){
+                    terminarJuego();
+                }
+
+            }else{
+                terminarJuego();
+            }
         }
     });
 });
 
 const escribirCasilla = (casilla,valor) => {
     casilla.innerHTML = valor;
-    actualizarPosiciones(casilla);
+    actualizarPosiciones(casilla,valor);
 };
 
-const actualizarPosiciones = (casilla) =>{
+const actualizarPosiciones = (casilla,valor) =>{
     posiciones.forEach((idPos , i) => {
         if(idPos == casilla.id){
-            tablero[i] = 'x';
+
+            const col = (casilla.id).substring(0,1);
+            const fila = (casilla.id).substring(1,2);
+
+            tablero[col][fila] = valor;
         }
     });
     posDisponibles.forEach( (valor,i) => {
         if(valor == casilla.id){
-            posDisponibles.splice(i,1);
+            posDisponibles.splice(i,1);//se borra una pos disp
         }
     });
     
@@ -65,3 +80,61 @@ boton.addEventListener('click', () => {
         casilla.innerHTML = '';
     })
 });
+
+
+
+const verificarGanador = (Jugador) => {
+    let hayGanador = true;
+    //verifica horizontal en las tres filas
+    for(let i=0; i<tablero.length; i++){
+        hayGanador=true;
+        for(let j=0; j<tablero.length; j++){
+            hayGanador = hayGanador && (tablero[i][j] == Jugador);
+        }
+        if(hayGanador){
+            return true;
+        }
+    }
+    
+
+    //verifica vertical
+    for(let i=0; i<tablero.length; i++){
+        hayGanador=true;
+        for(let j=0; j<tablero.length; j++){
+            hayGanador = hayGanador && (tablero[j][i] == Jugador);
+        }
+        if(hayGanador){
+            return true;
+        }
+    }
+
+
+    //verifica diagonal izq
+    hayGanador=true;
+    for(let i=0; i<tablero.length; i++){
+        hayGanador = hayGanador && (tablero[i][i] == Jugador);
+    }
+    if(hayGanador){
+        return true;
+    }
+
+    //verifica diagonal der
+    if(tablero[2][0]==Jugador && tablero[1][1]==Jugador && tablero[0][2]==Jugador){
+        return true;
+    }
+
+    return false;
+};
+
+
+const terminarJuego = () => {
+    console.log('termino el juego')
+    terminoJuego = true;
+}
+
+
+
+
+
+
+
