35c332e9690a0f539660342d75f672fb8889e623
leonelSubelza <subelza150@gmail.com>
2022-02-08 23:48:11 -0300

    Update app.js
    Se agregan avances con el bot rival

--- app/app.js
+++ app/app.js
@@ -2,17 +2,54 @@ const posiciones = ['00','01','02',
                     '10','11','12',
                     '20','21','22'];
 
+const tablero = ['','','',
+                 '','','',
+                 '','',''];
+
+const posDisponibles = ['00','01','02',
+                        '10','11','12',
+                        '20','21','22'];
+
 
 document.querySelectorAll(".casillero").forEach( casilla => {
     casilla.addEventListener('click', (e) => {
         
-        if(casilla.innerHTML == '' || casilla.innerHTML != 'x' || casilla.innerHTML != 'x'){
-            casilla.innerHTML = 'x';
-            return;
-        }
-        if(casilla.innerHTML == 'x'){
-            casilla.innerHTML = 'o'
-            return;
+        //en el caso de que se modifique el html a la fuerza y se escriba algo
+        if(casilla.innerHTML == '' && casilla.innerHTML != 'o' && casilla.innerHTML != 'x'){
+            escribirCasilla(casilla,'x');
+            setTimeout( () => {
+                botJugar();
+            },500);
         }
     });
 });
+
+const escribirCasilla = (casilla,valor) => {
+    casilla.innerHTML = valor;
+    actualizarPosiciones(casilla);
+};
+
+const actualizarPosiciones = (casilla) =>{
+    posiciones.forEach((idPos , i) => {
+        if(idPos == casilla.id){
+            tablero[i] = 'x';
+        }
+    });
+    posDisponibles.forEach( (valor,i) => {
+        if(valor == casilla.id){
+            posDisponibles.splice(i,1);
+        }
+    });
+    
+};
+
+
+const botJugar = () => {
+    const pos = elegirPosAleatoria();
+    const casilla = document.getElementById(posDisponibles[pos]);
+    escribirCasilla(casilla,'O');
+}
+
+const elegirPosAleatoria = () => {
+    return numeroRandom = Math.floor((Math.random() * (posDisponibles.length - 1)));
+}
